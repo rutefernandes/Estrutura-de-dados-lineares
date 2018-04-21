616a403a2cac9a9cea300b2ff70fa1ba3a2b2718
Tiago Lisboa <tiago.caio.ol@gmail.com>
2018-04-21 20:34:41 -0300

    agora vai a arvore certa, amém

---
+++ TAD-arvore-binaria-array/src/tad/arvore/binaria/NotFoundException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tad.arvore.binaria;
+
+/**
+ *
+ * @author tiago
+ */
+public class NotFoundException extends Exception {
+
+    public NotFoundException() {
+    }
+    
+}
